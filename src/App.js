3959a707443109aa3302666d8c8fcbb4cfbd2ae6
Hatice-Altun <van.altun@outlook.com>
2024-01-11 18:29:50 +0100

    Filter Yacht Example

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Filter from "./components/Filter/Filter";
+import "./App.css";
+
+// npm i sass-loader sass webpack --save-dev
+
+function App() {
+  return (
+    <div className="App">
+      <Filter/>
+    </div>
+  );
+}
+
+export default App;
